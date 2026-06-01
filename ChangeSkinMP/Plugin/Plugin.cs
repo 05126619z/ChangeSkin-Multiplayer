c854d386b8253352065f3871179eb9564f52ce17
Karlo Poljančić <call@me.nl>
2026-06-01 12:14:45 +0200

    version bump

--- ChangeSkinMP/Plugin/Plugin.cs
+++ ChangeSkinMP/Plugin/Plugin.cs
@@ -16,7 +16,7 @@ namespace ChangeSkinMP
     {
         public const string ModGUID = "05126619z.changeskin";
         public const string ModName = "ChangeSkin";
-        public const string ModVersion = "3.0.1";
+        public const string ModVersion = "3.1.0";
 
         internal static new ManualLogSource Logger;
         private readonly Harmony _harmony = new(ModGUID);
